80ee0c0294bcc2de95b2ad566dffc07745b89822
Bishode123 <34882617+Bishode123@users.noreply.github.com>
2017-12-29 14:58:24 +0530

    Add project files.

---
+++ VM/Models/ItemCommentsViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace VM.Models
+{
+    public class ItemCommentsViewModel
+    {
+
+        public ItemModel item { get; set; }
+        public List<BuyersCommentsModel> comments { get; set; }
+    }
+}
